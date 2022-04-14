36206be4ee8853359c93056c98786589967e71eb
ShakilJoy31 <joy31a@gmail.com>
2022-04-15 04:27:30 +0600

    Upper part of the home component is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import UpperHome from './Pages/Home/UpperHome/UpperHome';
 import Header from './Shered/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <UpperHome></UpperHome>
     </div>
   );
 }
---
+++ src/Pages/Home/UpperHome/UpperHome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import backgroundImg from '../../../images/bannerbackground.png'; 
+
+const UpperHome = () => {
+    return (
+        <div className='d-flex pt-5 justify-content-center' style={{backgroundImage: `url(${backgroundImg})`, backgroundRepeat: 'no-repeat', height: '800px'}}>
+            <div className='pt-5'>
+                <h1>Best Food Waiting for your Stomach</h1>
+                <div>
+                    <input style={{border: 'none'}} className='rounded-pill fs-3 ps-5' type="text" name="" id="" />
+                    <button style={{border: 'none'}} className='ps-4 pe-4 text-info bg-danger rounded-pill fs-3'>Search</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UpperHome;
